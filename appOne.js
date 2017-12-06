1b6f258d53566b266b2738525ad95434936c7406
michaelfrank <michaelfrankbusiness@gmail.com>
2017-12-06 13:04:12 -0500

    added images touched up nav bar

--- appOne.js
+++ appOne.js
@@ -5,6 +5,7 @@ var numbers = [];
 
 
 btnStart.addEventListener("click", function () {
+    printArea.innerHTML = "";
     for (var i = 1; i <= 100; i++) {
         if (i % 3 == 0 && i % 5 == 0) {
             printArea.innerHTML += "FIZZBUZZ<br>";
